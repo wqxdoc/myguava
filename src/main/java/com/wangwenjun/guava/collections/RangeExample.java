cd06c3dbe099030f2e3692201b6fa98afbebb1b0
wangqiang <874123073@qq.com>
2018-09-09 11:41:36 +0800

    init

---
+++ src/main/java/com/wangwenjun/guava/collections/RangeExample.java
@@ -0,0 +1,12 @@
+package com.wangwenjun.guava.collections;
+
+/***************************************
+ * @author:Alex Wang
+ * @Date:2018/1/14
+ * QQ: 532500648
+ * QQ群:463962286
+ ***************************************/
+public class RangeExample
+{
+
+}
